7da9b8680698a2f659435bc2555f5703e5cf47a2
akhiljayan29.aj <akhiljayan29.aj@gmail.com>
2021-03-19 04:24:46 +0530

    Router and Routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/hompage/homepage.component';
 
+const HatsPage = () => {
+	return (
+		<div>
+			<h1>Hats</h1>
+		</div>
+	);
+};
+
 function App() {
-	return <HomePage />;
+	return (
+		<div>
+			<Switch>
+				<Route exact path="/" component={HomePage} />
+				<Route exact path="/hats" component={HatsPage} />
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -12,33 +12,33 @@ class Directory extends React.Component {
 					title: 'HATS',
 					imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
 					id: 1,
-					linkUrl: 'shop/hats'
+					linkUrl: 'hats'
 				},
 				{
 					title: 'JACKETS',
 					imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
 					id: 2,
-					linkUrl: 'shop/jackets'
+					linkUrl: ''
 				},
 				{
 					title: 'SNEAKERS',
 					imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
 					id: 3,
-					linkUrl: 'shop/sneakers'
+					linkUrl: ''
 				},
 				{
 					title: 'WOMENS',
 					imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
 					size: 'large',
 					id: 4,
-					linkUrl: 'shop/womens'
+					linkUrl: ''
 				},
 				{
 					title: 'MENS',
 					imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
 					size: 'large',
 					id: 5,
-					linkUrl: 'shop/mens'
+					linkUrl: ''
 				}
 			]
 		};
@@ -47,8 +47,8 @@ class Directory extends React.Component {
 	render() {
 		return (
 			<div className="directory-menu">
-				{this.state.sections.map(({ title, imageUrl, size, id }) => (
-					<MenuItem title={title} key={id} imageUrl={imageUrl} size={size} />
+				{this.state.sections.map(({ id, ...otherSectionprops }) => (
+					<MenuItem {...otherSectionprops} key={id} />
 				))}
 			</div>
 		);
